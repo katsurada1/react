fa8874f993a65bdebf53eb6bee2f2b21a373c5ee
kasturada1 <kazuki1.katsurada@gmail.com>
2023-06-01 16:56:12 +0900

    Update ReactDOMEventListener-test

--- packages/react-dom-bindings/src/client/HTMLNodeType.js
+++ packages/react-dom-bindings/src/client/HTMLNodeType.js
@@ -6,4 +6,5 @@
  * HTML nodeType values that represent the type of the node
  */
 
+export const TEXT_NODE = 3;
 export const COMMENT_NODE = 8;
--- packages/react-dom-bindings/src/event/__tests__/ReactDOMEventListener-test.js
+++ packages/react-dom-bindings/src/event/__tests__/ReactDOMEventListener-test.js
@@ -1,5 +1,36 @@
-import { getEventPriority } from '../ReactDOMEventListener';
+import { JSDOM } from 'jsdom';
+
+import {
+  dispatchDiscreteEvent,
+  getEventPriority,
+} from '../ReactDOMEventListener';
 
 test('call test', () => {
   getEventPriority('message');
+});
+
+test('dispatchEvent tests', () => {
+  const domEventName = 'click';
+  const eventSystemFlags = { bubbles: true };
+  // const container = document.createElement('div');
+  const nativeEvent = { type: 'click' };
+  // Create a virtual DOM using JSDOM
+  const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
+  const { window } = dom;
+  const { document } = window;
+
+  // Create a button element
+  const myButton = document.createElement('button');
+  myButton.textContent = 'Click me';
+
+  // Append the button to the body of the virtual DOM
+  document.body.appendChild(myButton);
+
+  // Add an event listener to the button
+  myButton.addEventListener('click', () => {
+    console.log('Button clicked!');
+  });
+
+  // Call the function to be tested
+  dispatchDiscreteEvent(domEventName, eventSystemFlags, myButton, nativeEvent);
 });
